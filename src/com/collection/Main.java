9cea326c3dbfc8da5ad5cf452961d221c387348d
Vishal Mate <vishalmate27597@gmail.com>
2022-03-23 14:12:19 +0530

    Commiting the new Changes to test2 branch

--- src/com/collection/Main.java
+++ src/com/collection/Main.java
@@ -3,7 +3,7 @@ package com.collection;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World"); //comnentg gg
+        System.out.println("Hello World");
     }
 }
 
